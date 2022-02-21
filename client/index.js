c82537928717b2e7489ef67488dba0fb9490f929
TheGronne <kasperg102@gmail.com>
2022-02-21 10:19:11 +0100

    changed location

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
-Send();
-function Send(message){
+let messages = document.getElementById('chat_window');
+
+function Send(){
+    var message = document.getElementById('sendfield').value;
     var searchParams = new URLSearchParams({msg: message}).toString;
     var response = api_post("message/send", searchParams);
     GetMessages();
@@ -9,4 +11,18 @@ function GetMessages(){
     var response = api_get("messages/get");
     var parsedData = JSON.parse(response);
     console.log(parsedData);
+    
+    let msg = document.getElementById("sendfield").value
+    document.getElementById('chattext').innerHTML += '<p>' + msg + '</p>';
+    // document.getElementById('chattext').innerHTML += '<br>';
+    // api_post(msg);
+
+    shouldScroll = messages.scrollTop + messages.clientHeight === messages.scrollHeight;
+
+    if (!shouldScroll) {
+        scrollToBottom();
+    }
+    function scrollToBottom() {
+        messages.scrollTop = messages.scrollHeight;
+    }
 }
