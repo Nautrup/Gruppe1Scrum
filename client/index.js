e14837910fc31496a331dc5005f67db64bbe4c32
TheGronne <kasperg102@gmail.com>
2022-02-21 10:55:28 +0100

    autohrize kinda not done

--- client/index.js
+++ client/index.js
@@ -3,17 +3,17 @@ let messages = document.getElementById('chat_window');
 function Send(){
     var message = document.getElementById('sendfield').value;
     var searchParams = new URLSearchParams({msg: message}).toString;
-    var response = api_post("message/send", searchParams);
+    var response = api_post("messages/0", searchParams);
     GetMessages();
 }
 
 function GetMessages(){
-    var response = api_get("messages/get");
+    var response = api_get("messages/0");
     var parsedData = JSON.parse(response);
     console.log(parsedData);
     
     for (let i = 0; i < parsedData.length; i++) {
-        var msg = parsedData[i].message;
+        var msg = parsedData[1].content[i];
         document.getElementById('chattext').innerHTML += '<p>' + msg + '</p>';
     }
     // let msg = document.getElementById("sendfield").value
@@ -29,4 +29,23 @@ function GetMessages(){
     function scrollToBottom() {
         messages.scrollTop = messages.scrollHeight;
     }
+}
+
+function Login(){
+    var usern = document.getElementById('Username');
+    var passw = document.getElementById('Password');
+    var searchParams = new URLSearchParams({username: usern, password: passw}).toString;
+    var response = api_post("authorize/login", searchParams);
+}
+
+function Register(){
+    var usern = document.getElementById('Username');
+    var passw = document.getElementById('Password');
+    var searchParams = new URLSearchParams({username: usern, password: passw}).toString;
+    var response = api_post("user", searchParams);
+}
+
+function RenewJWT(){
+    var searchParams = new URLSearchParams({token: token}).toString;
+    var response = api_post("authorize/renew", searchParams);
 }
