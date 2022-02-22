fac4823ffb1ca12801600874609e32f45fd2637f
Kasper <kasper.2601@gmail.com>
2022-02-22 09:20:17 +0100

    Moved endpoint implementation to their own files

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const fs = require("fs")
 const express = require("express")
 const app = express()
 
+const endpoints = {
+    messages: require("./endpoints/messages.js"),
+}
+
 const config = JSON.parse(fs.readFileSync(`${__dirname}/config/server.json`))
 console.log("Server Config:")
 for(const [key, value] of Object.entries(config))
@@ -12,20 +16,6 @@ for(const [key, value] of Object.entries(config))
 }
 console.log("\n")
 
-/**
- * Removes the timezone offset from a date and returns it in ISO format.
- * 
- * Use this when passing a date to be used in a query
- * 
- * @param {Date} date The date to convert to servertime
- * 
- * @returns {string} The corrected date in ISO format
- */
-function servertime(date)
-{
-    const time = date.valueOf() - date.getTimezoneOffset() * 60 * 1000
-    return (new Date(time)).toISOString()
-}
 
 // Set the origin on all requests (CORS)
 app.use((req, res, next) => {
@@ -45,74 +35,14 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get("/messages/:ID", (req, res) => {
-    
-    const { channelID } = req?.params ?? {}
-    const { after, before, limit } = req?.query ?? {}
 
-    let filters = []
-    if(channelID == undefined)
-        filters.push(db.filters.isnull(db.tables.Messages.columns.Channel))
-    else
-        filters.push(db.filters.equals(db.tables.Messages.columns.Channel, channelID))
-    
-    if(after !== undefined && Number.isInteger(+after))
-        filters.push(db.filters.greaterequals(db.tables.Messages.columns.TimeSent, servertime(new Date(+after))))
+/////////////////////////////
+// Actual endpoints
+/////////////////////////////
 
-    if(before !== undefined && Number.isInteger(+before))
-        filters.push(db.filters.lessequals(db.tables.Messages.columns.TimeSent, servertime(new Date(+before))))
 
-    if(limit !== undefined && Number.isInteger(+limit))
-        filters.push(db.filters.limit(null, +limit))
-
-    res.append("Content-Type", "application/json")
-    db.select(db.tables.Messages, filters).then(rows => {
-        const messages = []
-        for(const row of rows)
-        {
-            messages.push({
-                id: row.ID,
-                content: row.Message,
-                timesent: new Date(row.TimeSent).valueOf(),
-                userid: row.UserID,
-                username: 'ikke udfyldt endnu',
-                channel: row.Channel,
-            })
-        }
-        res.send({ success: true, messages })
-    }).catch(reason => {
-        res.send({
-            success:  false,
-            messages: null,
-            error:    reason,
-        })
-    })
-})
-
-app.post("/messages/:ID", (req, res) => {
-    
-    const { channelID } = req?.params ?? {}
-    const { id, content } = req?.body ?? {}
-    
-    if(!content)
-        return void res.send({ success: false, error: `Message content was undefined` })
-
-    res.append("Content-Type", "application/json")
-    db.insert(db.tables.Messages, {
-        UserID: id,
-        Message: content,
-        Channel: channelID ?? null
-    }).then(result => {
-        res.send({
-            success: true,
-        })
-    }).catch(reason => {
-        res.send({
-            success: false,
-            error:   reason,
-        })
-    })
-})
+app.get("/messages/:ID", endpoints.messages.get(app, db))
+app.post("/messages/:ID", endpoints.messages.post(app, db))
 
 
 
